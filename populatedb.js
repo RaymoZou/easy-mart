30497aab0df77739db6b7cd90278d2f8ba971031
Raymond Zou <raymond.1zou@gmail.com>
2023-09-18 08:29:34 -0700

    add removal of Category collection

--- populatedb.js
+++ populatedb.js
@@ -4,6 +4,9 @@ const Category = require('./models/category')
 
 console.log("This script creates some categories to populate the database. Assumes a MONGODB_URI string in .env file.");
 
+// set this to false if you wish to retain the current data in the database
+const IS_RESET = true;
+
 // mongoose.set("strictQuery", false); // Prepare for Mongoose 7
 
 main().catch((err) => console.log(err));
@@ -12,10 +15,19 @@ async function main() {
     console.log("Debug: About to connect");
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("Debug: Should be connected?");
+    if (IS_RESET) await clear_collections();
     await createCategory();
     await createShoes();
     console.log("Debug: Closing mongoose");
     mongoose.connection.close();
+}
+
+async function clear_collections() {
+    try {
+        await Category.deleteMany({});
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 async function categoryCreate(name) {
