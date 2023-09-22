e3a7963ceba010aa96063f2d8ee44d97565671bf
Raymond Zou <raymond.1zou@gmail.com>
2023-09-21 23:04:26 -0700

    add POST route for updating items

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const Item = require("../models/item");
+const Category = require("../models/category");
 
 exports.item_detail = asyncHandler(async (req, res, next) => {
     const item = await Item.findById(req.params.id);
@@ -15,4 +16,21 @@ exports.item_form = asyncHandler(async (req, res, next) => {
         title: `Edit item: ${item.name}`,
         item,
     })
+})
+
+exports.item_update_post = asyncHandler(async (req, res, next) => {
+    const item = new Item({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        _id: req.params.id,
+    })
+
+    const updatedItem = await Item.findByIdAndUpdate(req.params.id, item, {});
+    res.redirect(updatedItem.url);
+})
+
+exports.item_delete = asyncHandler(async (req, res, next) => {
+    await Item.findByIdAndRemove(req.params.id);
+    res.redirect('/category');
 })
--- routes/catalog.js
+++ routes/catalog.js
@@ -5,7 +5,7 @@ var router = express.Router();
 const category_controller = require("../controllers/categoryController");
 const item_controller = require("../controllers/itemController");
 
-/* GET categories */
+// GET categories
 router.get('/', category_controller.index);
 
 router.get('/:id', category_controller.category_list);
@@ -13,5 +13,10 @@ router.get('/:id', category_controller.category_list);
 router.get('/item/:id', item_controller.item_detail);
 
 router.get('/item/:id/update', item_controller.item_form);
+
+router.get('/item/:id/delete', item_controller.item_delete);
+
+// POST categories
+router.post('/item/:id/update', item_controller.item_update_post);
 
 module.exports = router;
