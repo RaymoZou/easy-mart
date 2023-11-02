4cd6e71a3519a2010179574d45c98f2ed1081b64
Raymond Zou <raymond.1zou@gmail.com>
2023-11-02 11:19:05 -0700

    reformat

--- app.js
+++ app.js
@@ -3,37 +3,14 @@ require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-// var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var categoryRouter = require('./routes/catalog');
+const mongoose = require('mongoose');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/category', categoryRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// set up mongoose connection
-const mongoose = require('mongoose');
-// mongoose.set("strictQuery", false);
 
 async function main() {
   await mongoose.connect(process.env.MONGODB_URI);
@@ -41,16 +18,25 @@ async function main() {
 
 main();
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/category', categoryRouter);
+
+app.use((req, res, next) => {
+  next(createError(404, `Uh oh, sorry we couldn't find that page for you`));
+})
+
+app.use((err, req, res, next) => {
+  res.status(err.status).send(err.message);
+})
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`listening on port ${process.env.PORT || 3000}`);
